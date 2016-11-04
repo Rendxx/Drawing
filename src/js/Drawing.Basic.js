edd982b005de6c210346e562ad807e9ed1a35314
Rendxx <rendxx@gmail.com>
2016-11-04 16:12:29 -0500

    start editing

---
+++ src/js/Drawing.Basic.js
@@ -0,0 +1,32 @@
+﻿window.$$ = window.$$ || {};
+window.$$.draw = window.$$.draw || {};
+
+(function (DRAW) {
+	"use strict";
+
+	var Basic = function (container, para) {
+		this.container = container;
+		this.para = para;
+		this.image = null;
+	};
+	Basic.prototype = Object.create(null);
+	Basic.prototype.constructor = Basic;
+
+	Basic.prototype.copy = function () {
+		// copy the element and return the copy
+		var copy = new Basic(this.container, para);
+		copy.image = this.image;
+		return copy;
+	};
+
+
+	Basic.prototype.create = function () {
+		// (re)create the element
+	};
+
+	Basic.prototype.destroy = function () {
+		// destroy the element and release all memory
+	};
+
+	DRAW.Basic = Basic;
+})(window.$$.draw);
