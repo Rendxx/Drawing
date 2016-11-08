30e8bbeb27ebe0b1e17bacf352fdfc4fd5d65649
Rendxx <rendxx@gmail.com>
2016-11-07 20:50:53 -0600

    Add line

--- src/js/Drawing.Basic.js
+++ src/js/Drawing.Basic.js
@@ -7,25 +7,39 @@ window.$$.draw = window.$$.draw || {};
 	var Basic = function (container, para) {
 		this.container = container;
 		this.para = para;
-		this.image = null;
+		this._canvas = document.createElement('canvas');
+		this._ctx = this._canvas.getContext('2d');
 	};
 	Basic.prototype = Object.create(null);
 	Basic.prototype.constructor = Basic;
 
 	Basic.prototype.copy = function () {
 		// copy the element and return the copy
-		var copy = new Basic(this.container, para);
-		copy.image = this.image;
+	    var copy = new Basic(this.container, para);
+	    copy._ctx.drawImage(this._canvas, 0, 0);
 		return copy;
 	};
 
+	Basic.prototype.draw = function () {
+	    // draw element
+	};
 
 	Basic.prototype.clear = function () {
-		// (re)clear the canvas
+	    // clear the canvas
+	    this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+	};
+
+	Basic.prototype.getImage = function () {
+	    // get the canvas image
+	    var image = new Image();
+	    image.src = this._canvas.toDataURL();
+	    return image;
 	};
 
 	Basic.prototype.destroy = function () {
-		// destroy the element and release all memory
+	    // destroy the element and release all memory
+	    this._canvas = null;
+	    this._ctx = null;
 	};
 
 	DRAW.Basic = Basic;
--- src/js/Drawing.Line.js
+++ src/js/Drawing.Line.js
@@ -1,1 +1,53 @@
-﻿
+﻿window.$$ = window.$$ || {};
+window.$$.draw = window.$$.draw || {};
+
+(function (DRAW) {
+    "use strict";
+
+    var Line = function (container, para) {
+        DRAW.Basic.call(this, container, para);
+        this.start = null;
+        this.end = null;
+    };
+    Line.prototype = Object.create(DRAW.Basic.prototype);
+    Line.prototype.constructor = Line;
+
+    Line.prototype.copy = function () {
+        var copy = DRAW.Basic.prototype.copy.call(this);
+        copy.start = this.start;
+        copy.end = this.end;
+    };
+
+    Line.prototype.draw = function (start, end, para) {
+        // draw element
+        if (start == null || end == null) throw new Error("parameter error");
+        this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height);
+        this.start = start;
+        this.end = end;
+
+        var para = para || {};
+        var style = para.style || para.color || '#000000';
+        var image = para.image || null;
+        var thickness = para.thickness || 1;
+
+        var x = start[0] - end[0];
+        var y = start[1] - end[1];
+        var len = Math.sqrt(x*x+y*y);
+        var angle = Math.atan2(y, x);
+
+        this._ctx.translate(start[0], start[1]);
+        this._ctx.rotate(angle);
+        this._ctx.drawImage(image, 0, 0, thickness, len);
+        if (image != null) {
+            var ptrn = this._ctx.createPattern(image, 'repeat');
+            this._ctx.fillStyle = ptrn;
+        } else {
+            this._ctx.fillStyle = style;
+        }
+        this._ctx.fillRect(0, 0, thickness, len);
+        this._ctx.rotate(-angle);
+        this._ctx.translate(-start[0], -start[1]);
+    };
+
+    DRAW.Line = Line;
+})(window.$$.draw);
