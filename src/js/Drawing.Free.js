d69cac499a628f513b351420afbfe8e9f4a528e8
Rendxx <rendxx@gmail.com>
2016-11-08 18:04:02 -0600

    start working on free draw

--- src/js/Drawing.Free.js
+++ src/js/Drawing.Free.js
@@ -1,1 +1,26 @@
-﻿
+﻿window.$$ = window.$$ || {};
+window.$$.draw = window.$$.draw || {};
+
+(function (DRAW) {
+    "use strict";
+
+    var FreeDraw = function (container, para) {
+        DRAW.Basic.call(this, container, para);
+    };
+    FreeDraw.prototype = Object.create(DRAW.Basic.prototype);
+    FreeDraw.prototype.constructor = FreeDraw;
+
+    FreeDraw.prototype.copy = function () {
+        var copy = DRAW.Basic.prototype.copy.call(this);
+    };
+
+    FreeDraw.prototype.startDraw = function (para) {
+        // draw element
+        var para = para || {};
+        var style = para.color || '#000000';
+        var thickness = para.thickness || 1;
+
+    };
+
+    DRAW.FreeDraw = FreeDraw;
+})(window.$$.draw);
