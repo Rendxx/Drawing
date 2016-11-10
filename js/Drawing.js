910dc3eb29a9cbd64b8d668a1854737fec6cee26
Rendxx <rendxx@gmail.com>
2016-11-10 15:29:09 -0600

    Free drawing done

--- js/Drawing.js
+++ js/Drawing.js
@@ -1,4 +1,39 @@
 window.$$ = window.$$ || {};
+window.$$.func = window.$$.func || {};
+
+(function (FUNC) {
+    "use strict";
+    FUNC.offsetScreen = function (node) {
+        if (node === document.body) return {top:0, left:0};
+        var top = node.offsetTop,
+            left = node.offsetLeft;
+        node = node.parentNode;
+        while (node !== document.body) {
+            top -= node.scrollTop;
+            left -= node.scrollLeft;
+            node = node.parentNode;
+        }
+        return { top: top, left: left };
+    };
+    
+    FUNC.getMousePos = function (e, parent) {
+        var pos;
+        if (e.pageX == undefined) {
+            pos = [e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY];
+        } else {
+            pos = [e.pageX, e.pageY];
+        }
+        if (parent != undefined) {
+            var offset = FUNC.offsetScreen(parent);
+            pos[0] -= offset.left;
+            pos[1] -= offset.top;
+        }
+        return pos;
+    };
+
+})(window.$$.func);
+
+window.$$ = window.$$ || {};
 window.$$.draw = window.$$.draw || {};
 
 (function (DRAW) {
@@ -9,6 +44,8 @@ window.$$.draw = window.$$.draw || {};
 		this.para = para;
 		this._canvas = document.createElement('canvas');
 		this._ctx = this._canvas.getContext('2d');
+		this.container.appendChild(this._canvas);
+		this.resize();
 	};
 	Basic.prototype = Object.create(null);
 	Basic.prototype.constructor = Basic;
@@ -42,13 +79,19 @@ window.$$.draw = window.$$.draw || {};
 	    this._ctx = null;
 	};
 
+	Basic.prototype.resize = function () {
+	    // resize the canvas
+	    this._canvas.width = this.container.offsetWidth;
+	    this._canvas.height = this.container.offsetHeight;
+	};
+
 	DRAW.Basic = Basic;
 })(window.$$.draw);
 
 window.$$ = window.$$ || {};
 window.$$.draw = window.$$.draw || {};
 
-(function (DRAW) {
+(function (DRAW, FUNC) {
     "use strict";
 
     var FreeDraw = function (container, para) {
@@ -61,18 +104,7 @@ window.$$.draw = window.$$.draw || {};
     FreeDraw.prototype.startDrawing = function (para) {
         // draw element
         var para = para || {};
-        //var style = para.color || '#000000';
-        //var thickness = para.thickness || 1;
-        //var join = para.join || 'round';
-
-        //this._ctx.lineWidth = thickness;
-        //this._ctx.strokeStyle = style;
-        //this._ctx.lineJoin = join;
-
-        for (var i in para) {
-            this._ctx[i] = para[i];
-        }
-
+        for (var i in para) this._ctx[i] = para[i];
         this._ctx.beginPath();
         this._lastPos = null;
         this.container.addEventListener("mousemove", this._fn_draw, false);
@@ -80,34 +112,21 @@ window.$$.draw = window.$$.draw || {};
 
     FreeDraw.prototype.stopDrawing = function () {
         this._lastPos = null;
-        this.container.removeEventListener(evt, this._fn_draw, false);
+        this.container.removeEventListener("mousemove", this._fn_draw, false);
     };
 
     FreeDraw.prototype._setupBinding = function () {
         var that = this;
-
-        var _getMousePos = function (e) {
-            var pos;
-            if (e.pageX == undefined) {
-                pos = [e.originalEvent.touches[0].pageX, e.originalEvent.touches[0].pageY];
-            } else {
-                pos = [e.pageX, e.pageY];
-            }
-            var viewportOffset = that._canvas.getBoundingClientRect();
-            pos[1] -= viewportOffset.top;
-            pos[0] -= viewportOffset.left
-            return pos;
-        };
         
         this._lastPos = null;
         this._fn_draw = function (e) {
-            var mousePos = _getMousePos(e);
+            var mousePos = FUNC.getMousePos(e, this);
             if (that._lastPos === null) {
                 that._lastPos = mousePos;
                 that._ctx.moveTo(mousePos[0], mousePos[1]);
                 return false;
             }
-
+            console.log(mousePos);
             that._ctx.lineTo(mousePos[0], mousePos[1]);
             that._ctx.stroke();
             that._lastPos = mousePos;
@@ -115,6 +134,6 @@ window.$$.draw = window.$$.draw || {};
     };
 
     DRAW.freeDraw = FreeDraw;
-})(window.$$.draw);
+})(window.$$.draw, window.$$.func);
 
 //# sourceMappingURL=Drawing.js.map
